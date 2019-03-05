c37c5cd758160c8cf3c97bafba045672e31e0f43
Abdul Wahab Rafehi <absrafehi@gmail.com>
2019-03-05 21:11:08 +1100

    update to run unit tests.

---
+++ test.ts
@@ -0,0 +1,19 @@
+import 'zone.js/dist/long-stack-trace-zone';
+import 'zone.js/dist/async-test';
+import 'zone.js/dist/fake-async-test';
+import 'zone.js/dist/sync-test';
+import 'zone.js/dist/proxy';
+import 'zone.js/dist/jasmine-patch';
+import './app/parent.component.spec';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
+getTestBed().resetTestEnvironment();
+
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
